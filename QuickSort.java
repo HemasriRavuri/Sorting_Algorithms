8726b35fdf5c7baa5d77f49038a39c653af46198
Hema Sri <149699389+HemasriRavuri@users.noreply.github.com>
2024-03-27 22:06:20 +0530

    Added Quick sorting algorithm

---
+++ QuickSort.java
@@ -0,0 +1,34 @@
+
+import java.util.Arrays;
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,2,1};
+        quicksort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void quicksort(int[] arr,int low,int high){
+        if(low>=high){
+            return;
+        }
+        int s=low,e=high;
+        int m=s+(e-s)/2;
+        int pivot=arr[m];
+        while(s<=e) {
+            while (arr[s] < pivot) {
+                s++;
+            }
+            while (arr[e] > pivot) {
+                e--;
+            }
+            if(s<=e){
+                int temp=arr[s];
+                arr[s]=arr[e];
+                arr[e]=temp;
+                s++;
+                e--;
+            }
+        }
+        quicksort(arr,low,e);
+        quicksort(arr,s,high);
+    }
+}
